e30384f7aaae6b7d1a0279bb3bbea5971d0f64df
Rahul <rahulsati9969@gmail.com>
2023-06-16 14:29:37 +0530

    'ok'

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -14,6 +14,6 @@ const store = configureStore({
     }
 })
 
-export const server = `http://localhost:8000/api/v1`
+export const server = `https://ultimate-project-backend.vercel.app/api/v1`
 
 export default store;
